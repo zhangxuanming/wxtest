9c5cd98a6465d8aaa8afaff1f5b75c1c43aa9fb0
bor <muyuruhai@gmail.com>
2015-09-11 16:20:51 +1000

    init

--- jssdk.php
+++ jssdk.php
@@ -8,15 +8,12 @@ class JSSDK {
     $this->appSecret = $appSecret;
   }
 
-  public function getSignPackage($signUrl = null) {
+  public function getSignPackage() {
     $jsapiTicket = $this->getJsApiTicket();
 
-    if ($signUrl) {
-      $url = $signUrl;
-    } else {
-      $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-      $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    }
+    // 注意 URL 一定要动态获取，不能 hardcode.
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
     $timestamp = time();
     $nonceStr = $this->createNonceStr();
@@ -30,11 +27,11 @@ class JSSDK {
       "appId"     => $this->appId,
       "nonceStr"  => $nonceStr,
       "timestamp" => $timestamp,
-      "signature" => $signature,
       "url"       => $url,
-      // "rawString" => $string
+      "signature" => $signature,
+      "rawString" => $string
     );
-    return $signPackage;
+    return $signPackage; 
   }
 
   private function createNonceStr($length = 16) {
@@ -48,18 +45,18 @@ class JSSDK {
 
   private function getJsApiTicket() {
     // jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
-    $data = json_decode(file_get_contents("./cache/jsapi_ticket.json"));
+    $data = json_decode(file_get_contents("jsapi_ticket.json"));
     if ($data->expire_time < time()) {
       $accessToken = $this->getAccessToken();
       // 如果是企业号用以下 URL 获取 ticket
-       $url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
-//      $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
+      // $url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
+      $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
       $res = json_decode($this->httpGet($url));
       $ticket = $res->ticket;
       if ($ticket) {
         $data->expire_time = time() + 7000;
         $data->jsapi_ticket = $ticket;
-        $fp = fopen("./cache/jsapi_ticket.json", "w");
+        $fp = fopen("jsapi_ticket.json", "w");
         fwrite($fp, json_encode($data));
         fclose($fp);
       }
@@ -72,17 +69,17 @@ class JSSDK {
 
   private function getAccessToken() {
     // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
-    $data = json_decode(file_get_contents("./cache/access_token.json"));
+    $data = json_decode(file_get_contents("access_token.json"));
     if ($data->expire_time < time()) {
       // 如果是企业号用以下URL获取access_token
-       $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
-//      $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
+      // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
+      $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
       $res = json_decode($this->httpGet($url));
       $access_token = $res->access_token;
       if ($access_token) {
         $data->expire_time = time() + 7000;
         $data->access_token = $access_token;
-        $fp = fopen("./cache/access_token.json", "w");
+        $fp = fopen("access_token.json", "w");
         fwrite($fp, json_encode($data));
         fclose($fp);
       }
