0c9ae958d8dd402894a8808aedb958065d3e51e8
bor <muyuruhai@gmail.com>
2015-09-12 16:22:37 +1000

    init

--- checklog.php
+++ checklog.php
@@ -3,18 +3,24 @@
 	<body>
 		<h3>Call Log</h3>
 		<div style="background-color: #ffffff;color: #000000">
-			<?php echo file_get_contents('log/call.log') ?>
+			<pre>
+				<?php echo file_get_contents('log/call.log') ?>
+			</pre>
 		</div>
 		<br>
 		<h3>token Log</h3>
 		<div style="background-color: #1b6d85;color: #ffffff">
-			<?php echo file_get_contents('log/curlToken.log') ?>
+			<pre>
+				<?php echo file_get_contents('log/curlToken.log') ?>
+			</pre>
 		</div>
 
 		<br>
 		<h3>jsapi Log</h3>
 		<div style="background-color: green;color: #ffffff">
-			<?php echo file_get_contents('log/curlJSApi.log') ?>
+			<pre>
+				<?php echo file_get_contents('log/curlJSApi.log') ?>
+			</pre>
 		</div>
 	</body>
 </html>
